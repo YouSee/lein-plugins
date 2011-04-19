97039da67fe6634d77bcbcd6b64d9965e093fb52
Thomas Engelschmidt <thenge@yousee.dk>
2011-04-19 13:23:15 +0200

    bumped version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject lein-plugins  "1.0.2-SNAPSHOT"
-  :description "Lein plugin for starting LazyTest watch"
+(defproject yij/lein-plugins  "1.0.2"
+  :description "Lein plugins"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[lein-clojars "0.6.0"] [com.stuartsierra/lazytest "1.1.2"]]
