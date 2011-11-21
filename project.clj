b89099d89e9fdb40e929e5cea04ac2d74e23c92a
Thomas Engelschmidt <thenge@yousee.dk>
2011-11-21 10:41:56 +0100

    bumped version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject yij/lein-plugins  "1.0.2"
+(defproject yij/lein-plugins  "1.0.3"
   :description "Lein plugins"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[lein-clojars "0.6.0"] [com.stuartsierra/lazytest "1.1.2"]]
+                 ]
+  :dev-dependencies [[org.clojure/clojure-contrib "1.2.0"][lein-clojars "0.6.0"] [com.stuartsierra/lazytest "1.1.2"]]
   :eval-in-leiningen true)
