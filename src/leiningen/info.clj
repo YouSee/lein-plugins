0c0637403c3b675e526058e659ab13dc81726938
David Nielsen <david@graveyard.dk>
2012-08-29 15:25:23 +0300

    Update src/leiningen/info.clj
    using println instead if prn

--- src/leiningen/info.clj
+++ src/leiningen/info.clj
@@ -2,8 +2,8 @@
   (:use clojure.contrib.pprint))
 
 (defn info [project & args]
-  (cond (= (first args) "name") (prn (:name project))
-	(= (first args) "version") (prn (:version project))
+  (cond (= (first args) "name") (println (:name project))
+	(= (first args) "version") (println (:version project))
 	(= (first args) "dependencies") (pprint (:dependencies project))
 	(= (first args) "dev-dependencies") (pprint (:dev-dependencies project))	
-	(= (first args) "description") (prn (:description project))))
+	(= (first args) "description") (println (:description project))))
