8fc3af5180d647039ad986e09e93331a35acbaf2
Steen Larsen <steen666@gmail.com>
2012-09-05 18:02:13 +0200

    updated lein-clojars

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject yij/lein-plugins  "1.0.8"
+(defproject yij/lein-plugins  "1.0.9"
   :description "Lein plugins"
   :dependencies [[org.clojure/clojure "1.4.0"]]
 
-  :profiles {:dev {:dependencies [[lein-clojars "0.6.0"]
+  :profiles {:dev {:dependencies [[lein-clojars "0.9.1"]
                                   [com.stuartsierra/lazytest "1.1.2"]]}}
   
   :dev-dependencies [[lein-clojars "0.6.0"]
